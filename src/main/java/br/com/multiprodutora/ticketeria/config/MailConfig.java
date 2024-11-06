16a9c268a7b75cb57d46fee6e4670b854da19064
marcotulionasc <marconascpriv@gmail.com>
2024-11-06 20:24:37 -0300

    :sparkles: Adicionado Bean manual, pois o spring não é capaz o suficiente de criar

---
+++ src/main/java/br/com/multiprodutora/ticketeria/config/MailConfig.java
@@ -0,0 +1,35 @@
+package br.com.multiprodutora.ticketeria.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import java.util.Properties;
+
+@Configuration
+public class MailConfig {
+
+    @Value("${MAIL_USERNAME}")
+    private String username;
+
+    @Value("${MAIL_PASSWORD}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost("smtp.gmail.com");
+        mailSender.setPort(587);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+
+        return mailSender;
+    }
+}
