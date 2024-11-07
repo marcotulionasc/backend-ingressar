ca36ed7cd2983354737aadc0f7fead1e97aa5b96
marcotulionasc <marconascpriv@gmail.com>
2024-11-07 11:00:34 -0300

    :sparkles: Convertendo para String o objeto

--- src/main/java/br/com/multiprodutora/ticketeria/domain/model/payment/dto/TicketDTO.java
+++ src/main/java/br/com/multiprodutora/ticketeria/domain/model/payment/dto/TicketDTO.java
@@ -3,9 +3,11 @@ package br.com.multiprodutora.ticketeria.domain.model.payment.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class TicketDTO {
     @JsonProperty("ticketId")
     private Long ticketId;
