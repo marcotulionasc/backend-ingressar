b3d2a0084247de8eb9ea71c936f88dacf06b5dc2
marcotulionasc <marconascpriv@gmail.com>
2024-12-18 18:36:53 -0300

    :change: RequestParam lixo demais vei

--- src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
+++ src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -21,7 +22,7 @@ public class TicketUserController {
     Logger logger = Logger.getLogger(TicketUserController.class.getName());
 
     @GetMapping("/user-tickets/{userId}")
-    public ResponseEntity<List<Payment>> getPaymentsByUserId(String userId){
+    public ResponseEntity<List<Payment>> getPaymentsByUserId(@RequestParam String userId){
 
         logger.info("Buscando pagamentos do usuário: " + userId);
 
