36b7340cdf1ae24644723f21029b5cb74527891b
marcotulionasc <marconascpriv@gmail.com>
2024-12-18 18:46:40 -0300

    :fire: @PathVariable

--- src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
+++ src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
@@ -4,10 +4,7 @@ import br.com.multiprodutora.ticketeria.application.payment.service.PaymentServi
 import br.com.multiprodutora.ticketeria.domain.model.payment.Payment;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.logging.Logger;
@@ -22,7 +19,7 @@ public class TicketUserController {
     Logger logger = Logger.getLogger(TicketUserController.class.getName());
 
     @GetMapping("/user-tickets/{userId}")
-    public ResponseEntity<List<Payment>> getPaymentsByUserId(@RequestParam String userId){
+    public ResponseEntity<List<Payment>> getPaymentsByUserId(@PathVariable String userId){
 
         logger.info("Buscando pagamentos do usuário: " + userId);
 
