79022f3cd80ed46a5c0002c8270df61a1c695e34
marcotulionasc <marconascpriv@gmail.com>
2024-12-18 18:28:07 -0300

    :change: FODACIO

--- src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
+++ src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping("/api")
@@ -17,8 +18,12 @@ public class TicketUserController {
     @Autowired
     private PaymentService paymentService;
 
+    Logger logger = Logger.getLogger(TicketUserController.class.getName());
+
     @GetMapping("/user-tickets/{userId}")
     public ResponseEntity<List<Payment>> getPaymentsByUserId(String userId){
+
+        logger.info("Buscando pagamentos do usuário: " + userId);
 
         List<Payment> payments = paymentService.getPaymentsByUserId(userId);
         if(payments.isEmpty()){
