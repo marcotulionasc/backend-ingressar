c44b7c6ab1c36837af8ec6ab99ce39e26192802b
marcotulionasc <marconascpriv@gmail.com>
2024-12-18 18:59:16 -0300

    :fire: Transactional

--- src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
+++ src/main/java/br/com/multiprodutora/ticketeria/application/ticket/controller/TicketUserController.java
@@ -2,6 +2,7 @@ package br.com.multiprodutora.ticketeria.application.ticket.controller;
 
 import br.com.multiprodutora.ticketeria.application.payment.service.PaymentService;
 import br.com.multiprodutora.ticketeria.domain.model.payment.Payment;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,6 +20,7 @@ public class TicketUserController {
     Logger logger = Logger.getLogger(TicketUserController.class.getName());
 
     @GetMapping("/user-tickets/{userId}")
+    @Transactional
     public ResponseEntity<List<Payment>> getPaymentsByUserId(@PathVariable String userId){
 
         logger.info("Buscando pagamentos do usuário: " + userId);
